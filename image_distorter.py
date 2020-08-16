dc1adb64f1af0c42db50d32237f6b32e75c55e85
Thomas Lynn <thomaslynn01@gmail.com>
2020-08-16 16:56:32 +0100

    fixed l_noise and r_translation

--- image_distorter.py
+++ image_distorter.py
@@ -6,18 +6,19 @@ from scipy import ndimage
 def image_distorter(images, r_rotation, r_translation, r_l_noise):
     new_images = (images.clone()).numpy()
     
-    new_images = ndimage.rotate(new_images, -r_rotation+random.random()*2*r_rotation, axes = (3,2), reshape=False, order = 1)
+    for i in range(new_images.shape[0]):
+        new_images[i][0] = ndimage.rotate(new_images[i][0], -r_rotation+random.random()*2*r_rotation, axes = (1,0), reshape=False, order = 1)
     
-    x_delta = random.randint(-r_translation,r_translation)
-    y_delta = random.randint(-r_translation,r_translation)
-    
-    new_images = ndimage.shift(new_images,[0,0,y_delta,x_delta],order = 0)
+        x_delta = random.randint(-r_translation,r_translation)
+        y_delta = random.randint(-r_translation,r_translation)
+        
+        new_images[i][0] = ndimage.shift(new_images[i][0],[y_delta,x_delta],order = 0)
     
     l_noise = np.zeros((new_images.shape[0],new_images.shape[1],new_images.shape[2],new_images.shape[3]))
     for j in range(l_noise.shape[0]):
         for i in range(r_l_noise):
             l_noise[j][0][random.randint(0,l_noise.shape[2]-1)]\
-                [random.randint(0,l_noise.shape[3]-1)] = random.random()
+                [random.randint(0,l_noise.shape[3]-1)] = random.random()*2-1
 
     new_images+=l_noise
     
