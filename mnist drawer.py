fixed softmax

mnist drawer.py
@@ -10,7 +10,7 @@ if torch.cuda.is_available():
   device_id = "cuda:"+str(torch.cuda.device_count()-1)
 else:  
   device_id = "cpu" 
-device_id = "cpu"  
+#device_id = "cpu"  
 print("device id:",device_id)
 
 device = torch.device(device_id)
@@ -28,6 +28,7 @@ guesses = torch.zeros(10)
 image = torch.zeros((28,28))
 
 scale = 10
+softmax = torch.nn.Softmax(1)
 
 def draw_pixel(image,x,y,set_to):
     if x < 28 and x>=0 and y <28 and y>=0:
@@ -58,7 +59,7 @@ def draw_to_image(set_to,prev_pos,pos):
         draw_pixel(image,x/scale,y/scale+2,set_to/3)
         draw_pixel(image,x/scale+2,y/scale,set_to/3)
         draw_pixel(image,x/scale-2,y/scale,set_to/3)
-    guesses[:] = model(image.reshape(1,1,28,28).to(device))
+    guesses[:] = softmax(model(image.reshape(1,1,28,28).to(device)))
     #guesses[:] = model(image.reshape(1,1,28,28))
         
 

mnist_common.py
@@ -267,15 +267,15 @@ class ConvNet_8(torch.nn.Module):
     def __init__(self):
         super(ConvNet_8, self).__init__()
         self.layer1 = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 450, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(1, 650, kernel_size=3, stride=1, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = torch.nn.Sequential(
-            torch.nn.Conv2d(450, 600, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(650, 800, kernel_size=3, stride=1, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2, padding = 1))
         self.layer3 = torch.nn.Sequential(
-            torch.nn.Conv2d(600, 1000, kernel_size=3, stride=1, padding=1),
+            torch.nn.Conv2d(800, 1000, kernel_size=3, stride=1, padding=1),
             torch.nn.LeakyReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer4 = torch.nn.Sequential(
@@ -296,9 +296,9 @@ class ConvNet_8(torch.nn.Module):
         self.layer9 = torch.nn.Sequential(
             torch.nn.Conv2d(1000, 1000, kernel_size=3, stride=1, padding=1),
             torch.nn.LeakyReLU(),
-            torch.nn.MaxPool2d(kernel_size=4, stride=2, padding = 1))
+            torch.nn.MaxPool2d(kernel_size=4))
         self.drop_out = torch.nn.Dropout()
-        self.fc1 = torch.nn.Linear(2 * 2 * 1000, 1_000)
+        self.fc1 = torch.nn.Linear(1 * 1 * 1000, 1_000)
         self.s1 = torch.nn.LeakyReLU()
         self.fc2 = torch.nn.Linear(1_000, 10)
         

mnistmain.py
@@ -34,17 +34,18 @@ if load_model!=None:
         
 model.to(device)
 
-"""transform = torchvision.transforms.Compose([
+transform = torchvision.transforms.Compose([
     torchvision.transforms.RandomRotation(20,expand = True),
-    torchvision.transforms.RandomResizedCrop(28,scale = (0.7,1.3)),
+    torchvision.transforms.RandomResizedCrop(28,scale = (0.5,1.3)),
 #    torchvision.transforms.RandomGrayscale(),
     torchvision.transforms.ColorJitter(brightness = 0.05),
     torchvision.transforms.ToTensor()
-])"""
+])
+"""
 transform = torchvision.transforms.Compose([
     torchvision.transforms.RandomAffine(25,(0.2,0.2),(0.5,1.4)),
     torchvision.transforms.ToTensor()
-])
+])"""
 training_dataset = torchvision.datasets.MNIST("dataset/mnist_dataset", train=True, transform=transform, download=True)
 training_generator = torch.utils.data.DataLoader(training_dataset, batch_size = batch_size, shuffle=True)
 